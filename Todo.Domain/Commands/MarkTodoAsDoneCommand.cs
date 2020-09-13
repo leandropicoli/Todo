9ee4fc84c308444b9dd30be2ee1e25ef2482508a
Leandro Picoli <leandropicoli@live.com>
2020-09-13 13:55:56 -0300

    Create command to mark todo as done

--- Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand.cs
@@ -5,27 +5,25 @@ using Todo.Domain.Commands.Contracts;
 
 namespace Todo.Domain.Commands
 {
-    public class CreateTodoCommand : Notifiable, ICommand
+    public class MarkTodoAsDoneCommand : Notifiable, ICommand
     {
-        public CreateTodoCommand()
+        public MarkTodoAsDoneCommand()
         {
-        }
-        public CreateTodoCommand(string title, string user, DateTime date)
-        {
-            Title = title;
-            User = user;
-            Date = date;
         }
 
-        public string Title { get; set; }
+        public MarkTodoAsDoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
         public string User { get; set; }
-        public DateTime Date { get; set; }
         public void Validate()
         {
             AddNotifications(
                 new Contract()
                     .Requires()
-                    .HasMinLen(Title, 3, "Title", "Please, describe this task better.")
                     .HasMinLen(User, 6, "User", "Invalid user")
             );
         }
