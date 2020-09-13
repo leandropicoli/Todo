2230d688a1fee71c264144d79ac864693508375f
Leandro Picoli <leandropicoli@live.com>
2020-09-13 13:40:37 -0300

    Create command for Create Todos

---
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,27 @@
+using System;
+using Flunt.Notifications;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : Notifiable, ICommand
+    {
+        public CreateTodoCommand()
+        {
+        }
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            User = user;
+            Date = date;
+        }
+
+        public string Title { get; set; }
+        public string User { get; set; }
+        public DateTime Date { get; set; }
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
