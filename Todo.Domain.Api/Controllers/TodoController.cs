75a1f47c732753e74eddda0b7b86708d2eeee009
Leandro Picoli <leandropicoli@live.com>
2020-09-16 08:05:33 -0300

    Finished api routes

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,7 +1,11 @@
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
 using Todo.Domain.Handlers.Contracts;
+using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Api.Controllers
 {
@@ -11,9 +15,127 @@ namespace Todo.Domain.Api.Controllers
     {
 
         [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            return repository.GetAll("leandropicoli");
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetAllDone(user);
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetAllUndone(user);
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetByPeriod(
+                user,
+                DateTime.Now.Date,
+                true
+            );
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForToday(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetByPeriod(
+                user,
+                DateTime.Now.Date,
+                false
+            );
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetByPeriod(
+                user,
+                DateTime.Now.Date.AddDays(1),
+                true
+            );
+        }
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            var user = "leandropicoli";
+            return repository.GetByPeriod(
+                user,
+                DateTime.Now.Date.AddDays(1),
+                false
+            );
+        }
+
+        [Route("")]
         [HttpPost]
         public GenericCommandResult Create(
             [FromBody] CreateTodoCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            command.User = "leandropicoli";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update(
+            [FromBody] UpdateTodoCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            command.User = "leandropicoli";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone(
+            [FromBody] MarkTodoAsDoneCommand command,
+            [FromServices] TodoHandler handler
+        )
+        {
+            command.User = "leandropicoli";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone(
+            [FromBody] MarkTodoAsUndoneCommand command,
             [FromServices] TodoHandler handler
         )
         {
